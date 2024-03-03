bec56f566af5fbda3848618d7bacd1a831e4a9b0
Jozen <me@jozen.blue>
2024-03-03 20:55:11 +0800

    Restructure www

--- www/build.sh
+++ www/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-cd ../
-sh dist.sh
-
-cp zoom-platform.sh www/zoom-platform.sh
+rm -rf out/
+wget "https://github.com/ZOOM-Platform/zoom-platform.sh/releases/latest/download/zoom-platform.sh"
+mv zoom-platform.sh out/
+cp -r src/ out/
